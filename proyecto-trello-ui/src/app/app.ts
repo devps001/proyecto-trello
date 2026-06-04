16844a9b387d7251782a1f59dcd7cdf332fce3fe
devps001 <jean.pierre.alarcon.r@gmail.com>
2026-06-04 09:37:38 -0500

    feat: base API y UI conectados

---
+++ proyecto-trello-ui/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html'
+})
+export class App {
+  private http = inject(HttpClient);
+  data$ = this.http.get('http://localhost:5158/weatherforecast');
+}
